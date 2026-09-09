d962f6775a72375756fb9e4b5b09a54db875f29f
Arthur Ascedu <arthurascedu@proton.me>
2026-09-09 10:22:27 +0200

    added phpmyadmin, fix docker group error, update ip

--- roles/mariadb/files/script.sh
+++ roles/mariadb/files/script.sh
@@ -1,12 +1,6 @@
 #! /bin/sh
 set -e
 
-echo
-echo
-echo $SQL_ROOT_PASSWORD
-echo
-echo
-
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
@@ -25,14 +19,3 @@ FLUSH PRIVILEGES;"
 mariadb-admin -u root -p"$SQL_ROOT_PASSWORD" shutdown
 
 exec gosu mysql mariadbd --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
-
-# CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY test;
-# SET PASSWORD FOR root@localhost = PASSWORD(test);
-# GRANT ALL ON *.* TO root@localhost WITH GRANT OPTION;
-# CREATE USER IF NOT EXISTS root@'%' IDENTIFIED BY test;
-# SET PASSWORD FOR root@'%' = PASSWORD(test);
-# GRANT ALL ON *.* TO root@'%' WITH GRANT OPTION;
-# CREATE USER IF NOT EXISTS ${MARIADB_USER}@'%' IDENTIFIED BY ${MARIADB_PASSWORD};
-# SET PASSWORD FOR ${MARIADB_USER}@'%' = PASSWORD(${MARIADB_PASSWORD});
-# CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
-# GRANT ALL ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}@'%';
